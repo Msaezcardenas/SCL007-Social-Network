f421de711bd33e9f1076e8f7e088de489835d602
MarceArratiaF <marratiaf@hotmail.com>
2019-02-01 10:36:54 -0300

    agregando boton eliminar en main.js

--- src/main.js
+++ src/main.js
@@ -1,44 +1,61 @@
 //Para trabajar el DOM//
 // console.log("Hola")
 
-window.onload = inicializar;
-let formulario;
-let refMensajes;
-let fondoMensajes;
+window.onload = initialize;
 
-function inicializar(){
-    formulario = document.getElementById("formulario");
-    formulario.addEventListener("submit", enviarDatosAFirebase, false);
-    fondoMensajes = document.getElementById("fondoMensajes");
+let formMesagge;
+let refmessage;
+let messageBackground;
+//inicializa la conección entre base de datos y javascript
+function initialize(){
+    formMessage = document.getElementById("formMessage");
+    formMessage.addEventListener("submit", sendDataToFirebase, false);
+    messageBackground = document.getElementById("messageBackground");
+    initializeFirebase();
 
-    inicializarFirebase();
-
-    mostrarMensajesDeFirebase();
+    showMessageFromFirebase();
 
 }
-
-function mostrarMensajesDeFirebase(){
-    refMensajes = firebase.database().ref().child("mensaje");
-    refMensajes.on("value",function(snap){
+//mostrando mensaje de base de datos
+function showMessageFromFirebase(){
+    refmessage = firebase.database().ref().child("mensaje");
+    refmessage.on("value",function(snap){
         let todosLosMensajes = "";
-        document.getElementById("fondoMensajes").innerHTML ="";
+        document.getElementById("messageBackground").innerHTML ="";
         datos = snap.val();
+       
         for(var key in datos){
-            todosLosMensajes += "</br>" + datos[key].Nombre + " : " + datos[key].Mensaje;
+            if(datos[key].Eliminado === 0){ 
+             todosLosMensajes += "</br>" + datos[key].Nombre + " : " + datos[key].Mensaje+" <input type='button' value='X' onclick=updateDelete('"+key+"')>";
         }
-      //  fondoMensajes.innerHTML = todosLosMensajes;
-        document.getElementById("fondoMensajes").innerHTML += todosLosMensajes;
+        if(datos[key].Principal === 1){
+
+
+
+        }
+        }
+      //  messageBackground.innerHTML = todosLosMensajes;
+     document.getElementById("messageBackground").innerHTML += todosLosMensajes;
     })
 }
-
-
-function enviarDatosAFirebase(event){
-    event.preventDefault();
-    refMensajes.push({Mensaje: event.target.mensaje.value, Nombre:event.target.nombre.value});
-    document.getElementsByName("nombre").value="";
-    document.getElementsByName("mensaje").value="";
+//cambia estado del mensaje(actualiza si la persona borra)
+function updateDelete(valor){
+    refmessage = firebase.database().ref().child("mensaje").child(valor);
+    console.log(refmessage);
+    refmessage.update({
+    Eliminado:1    
+    });
 }
-function inicializarFirebase(){
+
+//Envía datos a Firebase
+function sendDataToFirebase(event){
+    let email = "a@a.cl";
+    event.preventDefault();
+    refmessage.push({Mensaje: event.target.mensaje.value, Nombre:event.target.nombre.value, Eliminado:0,Principal:0,Correo:email,Like:0});
+    
+}
+ //Parámetros para conexión de base de datos
+function initializeFirebase(){
   // Initialize Firebase
 	let config = {
 		apiKey: 'AIzaSyB-jbfNQ2raBjBe0Y8iDER0k1VVQIYx01M',
