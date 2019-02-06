439de54840130600b7be7b3cbe81a80db6d17de6
ElizabethCG <elizabeth.caceres@gmail.com>
2019-02-06 18:31:21 -0300

    borra archivos

--- js/dataEC.js
+++
@@ -1,9 +0,0 @@
-//PARA PROBAR VINCULOS. OK
-// console.log("holases");
-// document.getElementById('root').innerHTML += '<p> holases </p>';
-
-
-// const country = () => {
-//   let contador = 2;
-//   return contador;
-// }
