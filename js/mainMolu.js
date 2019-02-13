ea9ea52adfc41f37e34f866a96cd9778a3a11992
MarceArratia <marratiaf@hotmail.com>
2019-02-13 10:15:40 -0300

    corrigiendo comportamiento en comentarios en data.js

--- js/mainMolu.js
+++ js/mainMolu.js
@@ -37,7 +37,7 @@ function login(){
         document.getElementById("perfilUser").style.display = "block";
         let extension;
         //se agrega codigo para buscar el usuario en la tabla de users
-        refmessage=firebase.database().ref().child("users");
+        let refmessage=firebase.database().ref().child("users");
         refmessage.on("value",function(snap){
         let datos=snap.val();
         let key;
